1c0dfa1770f1602fee3b2e0008bf71f057f4afee
jwritz <29800465+jwritz@users.noreply.github.com>
2020-08-18 19:33:47 -0500

    Added example keybinds

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@ Config.keybinds = {
         category='General',
         keys={
             {description='Open/Close phone', key='F1'},
+            {description='Open/Close inventory', key='F2'},
         },
     },
     {
